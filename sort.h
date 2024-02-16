6d101e37f22eca763fede9031a41cfc4a543c6e7
Comfort99 <shingangecomfort@gmail.com>
2024-02-16 01:51:19 -0800

    bubble

--- sort.h
+++ sort.h
@@ -1,7 +1,9 @@
 #ifndef sort
 #define sort
+
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -16,8 +18,12 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+
 #endif /*sort*/
