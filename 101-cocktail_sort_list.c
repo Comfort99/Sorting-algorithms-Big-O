ee5083bd254f0ad7e07ec997c736e924c6e0bba5
MercyAkpan <akpanmeme@gmail.com>
2024-02-20 12:23:34 +0100

    alx

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,13 +1,9 @@
 #include "sort.h"
-/**
-  *
-  *
-  *
-  *
-  *
-  *
-  */
 void swap_(listint_t *y, listint_t *z);
+/**
+  * cocktail_sort_list - This sorts based on cocktail algorithm
+  * @list: This is the list to be sorted
+  */
 void cocktail_sort_list(listint_t **list)
 {
 	int flag = 1;
@@ -16,17 +12,22 @@ void cocktail_sort_list(listint_t **list)
 
 	if (list == NULL || *list == NULL)
 		return;
-	start = end =  *list;
-	while(end->next != NULL) /* i.e last node is end->next != NULL*/
+	start = end = *list;
+	if (start->next == NULL)
+		return;
+	while (end->next != NULL) /* i.e last node is end->next != NULL*/
 		end = end->next;
-	while (flag == 1){
+	while (flag == 1)
+	{
 		flag = 0;
-		for (; start->next != NULL && start != end;){
-			if (start->n > start->next->n){
+		for (; start->next != NULL && start != end;)
+		{
+			if (start->n > start->next->n)
+			{
 				flag = 1;
 				swap_(start, start->next); /* to swap what start points to	*/
 				update_list(list);
-  				print_list1(*list);
+				print_list1(*list);
 			}
 			else
 				start = start->next;
@@ -37,22 +38,23 @@ void cocktail_sort_list(listint_t **list)
 			break;
 	flag = 0;
 	end = end->prev;
-	
-	for (start = end->prev; start != NULL;) /*for loop Back*/{
-		if (start->n > start->next->n){
+
+	for (start = end->prev; start != NULL;) /*for loop Back*/
+	{
+		if (start->n > start->next->n)
+		{
 			flag = 1;
 			swap_(start, start->next);
-		 	update_list(list);
+			update_list(list);
 			print_list1(*list);
 		}
 
-		else  
+		else
 		if (start->prev == NULL)
 			break;
 		start = start->prev;
-		
+
 	} /* for loop*/
-	
 	/* for loop */
 	/*update_list(list);*/
 	/*print_list1(*list);*/
