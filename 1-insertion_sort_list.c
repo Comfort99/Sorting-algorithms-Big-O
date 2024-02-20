e877ee5a2f01058eaa610d398dc74095e0851ef0
MercyAkpan <akpanmeme@gmail.com>
2024-02-20 12:40:57 +0100

    alx

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,87 +4,40 @@
   * @list: This a double ptr to thr linked list
   * Return: No Return.
   */
-void update_list(listint_t **list);
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *t = *list;
-	listint_t *i;
-	listint_t *j;
+	listint_t *curr, *swap, *tmp;
 
-	j = *list;
 	if (!list || *list == NULL)
 		return;
-	if (j->next == NULL)
+	curr = *list;
+	/* validate if there is only one element in list */
+	if (curr->next == NULL)
 		return;
-	t = t->next;
-	/* t is at 48*/
-	/**
-	  *printf("This is t prev:%d\n", t->prev->n);
-	* printf("This is t:%d\n", t->n);
-	* printf("This is t next:%d\n", t->next->n);
-	*/
-	for (i = t->prev;  t != NULL; )
+	while (curr->next != NULL)
 	{
-		i = t->prev;
-	/*
-		*printf("============\n");
-		*printf("THIS is i:%d\n", i->n);
-		*printf("THIS is t:%d\n", t->n);
-		*/
-		while (i != NULL && (i->n > t->n))
+		swap = curr->next;
+		/* Comparison starts here */
+		if (curr->n > swap->n)
 		{
-			i = t->prev;
-			/*
-		*	printf("This is i1: %d\n", i->n);
-		*	printf("This is t: %d\n", t->n);
-			*/
-			i->next = t->next;
-			if (t->next != NULL)
+			tmp = curr;
+			while (tmp != NULL && tmp->n > swap->n)
 			{
-			/*	printf("i->next->n: %d\n", i->next->n);*/
-				t->next->prev = t->prev;
-			/*	printf("t->next->prev :%d\n", t->next->prev->n);*/
+				tmp->next = swap->next;
+				if (tmp->next != NULL)
+					tmp->next->prev = tmp;
+				swap->prev = tmp->prev;
+				if (swap->prev != NULL)
+					swap->prev->next = swap;
+				else
+					*list = swap;
+				tmp->prev = swap;
+				swap->next = tmp;
+				print_list(*list);
+				tmp = swap->prev;
 			}
-			if (i->prev != NULL)
-			{
-				t->prev = i->prev;
-/*			printf("This is t->prev: %d\n", t->prev->n);*/
-			}
-			else
-				t->prev = NULL;
-			i->prev = t;
-/*			printf("This is i->prev: %d\n", i->prev->n);*/
-			if (t->prev != NULL)
-			{
-				t->prev->next = t;
-/*			printf("This is t->prev->next: %d\n", t->prev->next->n);*/
-			}
-				t->next = i;
-/*				printf("This is t->next: %d\n", t->next->n);*/
-				i = t->prev;
-				/*if (i != NULL)*/
-/*					printf("This is i2 : %d\n", i->n);*/
-		 update_list(list);
-		 print_list1(*list);
-		} /*while loop*/
-		t = t->next;
-	} /*for loop*/
-
-	/*
-	   *j = *list;
-	*while (j->prev != NULL)
-	{
-		printf("j: %d\n", j->n);
-		if (j->prev != NULL)
-			printf("j->prev: %d\n", j->prev->n);
-		printf("j->next: %d\n", j->next->n);
-		j = j->prev;
+			continue;
+		}
+		curr = curr->next;
 	}
-	printf("END ======== ");
-	printf("j is finally : %d: \n", j->n);
-	*list = j;
-		printf("TRIAL WITH J \n");
-	*/
-	update_list(list);
-
-} /*end of fn*/
+}
