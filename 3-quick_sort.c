a4c8685529ded903593aa17fc2b5841027c2ef96
MercyAkpan <akpanmeme@gmail.com>
2024-02-19 18:22:19 -0800

    alx

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,91 +1,91 @@
 #include "sort.h"
 
-void swap(int *p, int *p1);
-void quicksort_recursive(int a[], int first, int last, int length);
-int partition(int a[], int first, int last, int length);
 /**
-  * quick_sort - This is quick sort
-  * @array: This is array
-  * @size: This is length of array
-  */
-
-void quick_sort(int *array, size_t size)
+ * myswap - swaps two values
+ *
+ * @array: data sort input
+ * @firts: first
+ * @second: second
+ * @size: size
+ *
+ * Return: No Return
+ */
+void myswap(int *array, int firts, int second, int size);
+void myquicksort(int *array, int left, int right, size_t size);
+int part(int *array, int left, int right, size_t size);
+void myswap(int *array, int firts, int second, int size)
 {
-	int length = size - 1;
+	int tmp;
 
-	if  (size < 2)
-		return;
-
-	quicksort_recursive(array, 0, size - 1, length);
-	print_array1(array, 0, length  + 1);
-}
-/**
-  * swap - This swaps elements
-  * @p: This is 1st
-  *@p1: This is 2nd
-  */
-void swap(int *p, int *p1)
-{
-	int temp = *p;
-	*p = *p1;
-	*p1 = temp;
-}
-/**
-  * quicksort_recursive - This is recursive .
-  * @a: This array
-  * @first: This is 1st index
-  * @last: This is last index
-  * @length: This is length
-  */
-void quicksort_recursive(int a[], int first, int last, int length)
-{
-
-	if (first < last)
+	if (array[firts] != array[second])
 	{
-	int i_p = partition(a, first, last, length); /*This is the pivot index*/
-/*	print_array1(a, 0, length  + 1);*/
-	quicksort_recursive(a, first, i_p - 1, length);
-/*	print_array1(a, 0, length  + 1);*/
-	quicksort_recursive(a, i_p + 1, last, length);
+		tmp = array[firts];
+		array[firts] = array[second];
+		array[second] = tmp;
+		print_array(array, size);
 	}
 }
-/**
-  * partition - This partitions
-  * @a: This is array
-  * @first: This is first index
-  * @last: This is last index
-  * @length: This is length
-  * Return: THIS returns pivot index.
-  */
-int partition(int a[], int first, int last, int length)
-{
-	int i_pv = a[last];
-	int i = first;/*i_pv is the value at the pivot index*/
-	int j = last, flag = 0;
 
-/*	printf("first: %d   ", first);*/
-/*	printf("last: %d    \n", last);*/
-	while (i < j)
+/**
+ * part - part to a pivot
+ *
+ * @array: data sort input
+ * @left: left
+ * @right: right
+ * @size: size input
+ *
+ * Return: New pivote
+ */
+int part(int *array, int left, int right, size_t size)
+{
+	int i = left, j, pivot  = array[right];
+
+	for (j = left; j <= right; j++)
 	{
-		while (a[i] < i_pv && i <= last)
+		if (array[j] < pivot)
 		{
+			myswap(array, i, j, size);
 			i++;
 		}
-		while (a[j] >= i_pv && j >= first)
-		{
-			j--;
-		}
-		if (i < j)
-		{
-			flag = 1;
-			swap(&a[i], &a[j]);
-			print_array1(a, 0, length  + 1);
-/*			printf("OUT OF IF\n");*/
-		}
 	}
-	swap(&a[last], &a[i]);
-	if (flag == 0)
-		print_array1(a, 0, length  + 1);
-/*	printf("=== RETURN===i: %d\n", i);*/
+	myswap(array, i, right, size);
+
 	return (i);
+}
+
+/**
+ * myquicksort -  quick sort algorithm
+ *
+ * @array: data to sort
+ * @left: left pivote
+ * @right: right pivote
+ * @size: size input
+ * Return: No Return
+ */
+void myquicksort(int *array, int left, int right, size_t size)
+{
+	int pivote;
+
+	if (left < right)
+	{
+		pivote = part(array, left, right, size);
+		myquicksort(array, left, pivote - 1, size);
+		myquicksort(array, pivote + 1, right, size);
+	}
+}
+
+/**
+ * quick_sort -  quick sort algorithm
+ *
+ * @array: sort data
+ * @size: size input
+ *
+ * Return: No Return
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (size < 2)
+		return;
+
+	myquicksort(array, 0, size - 1, size);
 }
