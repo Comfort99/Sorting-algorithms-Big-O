5a43deef2b5e67f2b19104e6601db0b4e281150f
MercyAkpan <akpanmeme@gmail.com>
2024-02-20 12:52:33 +0100

    alx

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,12 +8,8 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr, *swap, *tmp;
 
-	if (!list || *list == NULL)
-		return;
 	curr = *list;
 	/* validate if there is only one element in list */
-	if (curr->next == NULL)
-		return;
 	while (curr->next != NULL)
 	{
 		swap = curr->next;
